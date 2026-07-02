24c6b904e37bc761b2999588c8c90bd66ab3d4a8
André Lopes <andre.cristhian.lopes@escola.pr.gov.br>
2026-07-02 12:32:53 +0000

    ...

--- perguntas.js
+++ perguntas.js
@@ -8,13 +8,110 @@ criaCartao(
 )
 
 criaCartao(
-    'Disciplina 2',
-    'Pergunta 2',
-    'Resposta 2'
+    'Programação',
+    'O que é um algorítimo?',
+    'é uma sequência de passos para resolver um problema'
 )
 
+criaCartao(
+    'Matemática'
+    'O que é uma fração?',
+    'È uma forma de representar partes de um todo'
+)
+criaCartao(
+    'Matemática',
+    'O que é um número primo?',
+    'É um número que tem apenas dois divisores: 1 e ele mesmo.'
+)
+criaCartao(
+    'socilogia ? ',
+    '',
+    'Resposta 3'
+)
 criaCartao(
     'Disciplina 3',
     'Pergunta 3',
     'Resposta 3'
 )
+criaCartao(
+    'Disciplina 3',
+    'Pergunta 3',
+    'Resposta 3'
+)
+criaCartao(
+    'Disciplina 3',
+    'Pergunta 3',
+    'Resposta 3'
+)
+criaCartao(
+    'Disciplina 3',
+    'Pergunta 3',
+    'Resposta 3'
+)
+criaCartao(
+    'Disciplina 3',
+    'Pergunta 3',
+    'Resposta 3'
+)
+criaCartao(
+    'Disciplina 3',
+    'Pergunta 3',
+    'Resposta 3'
+)
+criaCartao(
+    'Disciplina 3',
+    'Pergunta 3',
+    'Resposta 3'
+)
+criaCartao(
+    'Disciplina 3',
+    'Pergunta 3',
+    'Resposta 3'
+)
+criaCartao(
+    'Disciplina 3',
+    'Pergunta 3',
+    'Resposta 3'
+)
+criaCartao(
+    'Disciplina 3',
+    'Pergunta 3',
+    'Resposta 3'
+)
+criaCartao(
+    'Disciplina 3',
+    'Pergunta 3',
+    'Resposta 3'
+)
+criaCartao(
+    'Disciplina 3',
+    'Pergunta 3',
+    'Resposta 3'
+)
+criaCartao(
+    'Disciplina 3',
+    'Pergunta 3',
+    'Resposta 3'
+)
+criaCartao(
+    'Disciplina 3',
+    'Pergunta 3',
+    'Resposta 3'
+)
+criaCartao(
+    'Disciplina 3',
+    'Pergunta 3',
+    'Resposta 3'
+)
+criaCartao(
+    'Disciplina 3',
+    'Pergunta 3',
+    'Resposta 3'
+)
+criaCartao(
+    'Disciplina 3',
+    'Pergunta 3',
+    'Resposta 3'
+)
+
+
