24387a58512a35802c12c0f42ee8ee6490fb59b4
André Lopes <andre.cristhian.lopes@escola.pr.gov.br>
2026-07-09 09:32:53 -0300

    ..

--- perguntas.js
+++ perguntas.js
@@ -25,93 +25,143 @@ criaCartao(
 )
 criaCartao(
     'socilogia ? ',
+    'O que é um fato social?',
+    'são as regras invisíveis da sociedade q molda como nós agimos '
+)
+criaCartao(
+    'Sociologia',
+    'que é "etnocentrismo" e qual é o seu conceito oposto na sociologia?',
+    'é julgar o outro a partir da própria cultura, considerando o seu grupo superior.'
+)
+criaCartao(
+    'Filosofia',
+    'O que é o "Mito da Caverna" de Platão?',
+    'A maioria das pessoas vive na ignorância (vendo "sombras"), e a filosofia serve para libertar a mente em direção à verdadeira realidade.'
+)
+criaCartao(
+    'Filosofia',
+    'que significa a frase "Penso, logo existo" de René Descartes?',
+    'Penso, logo existo" (Descartes): A única certeza absoluta é a própria dúvida; se eu penso e duvido, eu existo.'
+)
+criaCartao(
+    'Robótica',
+    'O que são os chamados "atuadores" em um sistema robótico e qual é a sua função?',
+    'Atuadores: "Músculos" do robô que transformam energia em movimento mecânico.'
+)
+criaCartao(
+    'Robótica',
+    'O que estabelecem as Três Leis da Robótica de Isaac Asimov e qual a sua importância histórica?',
+    'Leis da Robótica: Regras de segurança e obediência para proteger humanos, que iniciaram o debate ético sobre IA.'
+)
+criaCartao(
+    'Física ',
+    'O que é a Primeira Lei de Newton (Lei da Inércia)?',
+    'A matéria tende a manter seu estado atual (parada ou em movimento) se nenhuma força a obrigar a mudar.'
+)
+criaCartao(
+    'Potuguês',
+    'O que é uma palavra paroxítona?',
+    'É aquela cuja sílaba tônica (a pronunciada com mais força) é a penúltima, como em "cadeira", "lápis" ou "felicidade".'
+)
+criaCartao(
+    'Potuguês',
+    'Qual é a diferença entre "mal" e "mau"?',
+    '"Mal" é o oposto de "bem" (advérbio ou substantivo) e "mau" é o oposto de "bom" (adjetivo).',
+
+)
+criaCartao(
+    'inglês',
+    'Qual é a diferença entre "your" e "you're"?',
+    '"Your" é um possessivo (teu/seu),'
+)
+criaCartao(
+    'inglês',
+    'Quando se usa "since" e "for" no Present',
+    'Since vs. For: Since marca o início de um período (desde); for marca a duração (por/há).'
+)
+criaCartao(
+    'fisica e tecnociência',
+    'O que é a Computação Quântica (tecnociência)?',
+    'Uso da física quântica para criar computadores ultrarrápidos'
+)
+criaCartao(
+    'fisica e tecnociência',
+    'O que é o Efeito Fotoelétrico (física) e sua aplicação tecnológica?',
+    'Luz gerando eletricidade, base da energia solar.'
+)
+criaCartao(
+    'História',
+    'O que foi a Queda do Império Romano do Ocidente (476 d.C.)?',
+    'Fim do Império Romano por invasões e crises, iniciando a Idade Média.'
+)
+criaCartao(
+    'História',
+    'Qual foi a principal causa da Revolução Francesa (1789)?',
+    'Revolta do povo contra a desigualdade e os privilégios da nobreza.'
+)
+criaCartao(
     '',
-    'Resposta 3'
+    '',
+    ''
 )
 criaCartao(
-    'Disciplina 3',
-    'Pergunta 3',
-    'Resposta 3'
+    '',
+    '',
+    ' '
 )
 criaCartao(
-    'Disciplina 3',
-    'Pergunta 3',
-    'Resposta 3'
+    '',
+    '',
+    ''
 )
 criaCartao(
-    'Disciplina 3',
-    'Pergunta 3',
-    'Resposta 3'
+    '',
+    '',
+    ''
 )
 criaCartao(
-    'Disciplina 3',
-    'Pergunta 3',
-    'Resposta 3'
+    '',
+    '',
+    ''
 )
 criaCartao(
-    'Disciplina 3',
-    'Pergunta 3',
-    'Resposta 3'
+    '',
+    ''
 )
 criaCartao(
-    'Disciplina 3',
-    'Pergunta 3',
-    'Resposta 3'
+    '',
+    '',
+    ''
 )
 criaCartao(
-    'Disciplina 3',
-    'Pergunta 3',
-    'Resposta 3'
+    '',
+    '',
+    ''
 )
 criaCartao(
-    'Disciplina 3',
-    'Pergunta 3',
-    'Resposta 3'
+    '',
+    '',
+    ''
 )
 criaCartao(
-    'Disciplina 3',
-    'Pergunta 3',
-    'Resposta 3'
+    ' ',
+    '',
+    ''
 )
 criaCartao(
-    'Disciplina 3',
-    'Pergunta 3',
-    'Resposta 3'
+    '',
+    '',
+    ''
 )
 criaCartao(
-    'Disciplina 3',
-    'Pergunta 3',
-    'Resposta 3'
+    '',
+    '',
+    ''
 )
 criaCartao(
-    'Disciplina 3',
-    'Pergunta 3',
-    'Resposta 3'
-)
-criaCartao(
-    'Disciplina 3',
-    'Pergunta 3',
-    'Resposta 3'
-)
-criaCartao(
-    'Disciplina 3',
-    'Pergunta 3',
-    'Resposta 3'
-)
-criaCartao(
-    'Disciplina 3',
-    'Pergunta 3',
-    'Resposta 3'
-)
-criaCartao(
-    'Disciplina 3',
-    'Pergunta 3',
-    'Resposta 3'
-)
-criaCartao(
-    'Disciplina 3',
-    'Pergunta 3',
-    'Resposta 3'
+    '',
+    '',
+    ' '
 )
 
 
